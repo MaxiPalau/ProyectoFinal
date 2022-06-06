e3bd7e4f02b517ee89e8d2107733386167a32f8d
MartinQM <martin.quesada@gmail.com>
2022-06-05 21:30:11 -0300

    Search Form Images

--- productos/urls.py
+++ productos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from productos.views import productos, search_product_view
+from django.conf import settings
+from django.conf.urls.static import static
+from productos.views import productos, search_product_view, create_product_view
 
 urlpatterns = [
     path('', productos, name = 'productos'),
-    path('search-product/', search_product_view, name = 'search_product_view')
+    path('search-product/', search_product_view, name = 'search_product_view'),
+    path('create-product/', create_product_view, name = 'create-product'),
 ]
 
